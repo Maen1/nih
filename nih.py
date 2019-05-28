5b3445c7e068b4dd6f91d32f663847db52e844b0
Maen1 <mk.maen93@gmail.com>
2019-05-29 04:09:47 +0800

    updat path

--- nih.py
+++ nih.py
@@ -3,13 +3,13 @@ from glob import *
 import pandas as pd
 
 # path
-PATH = os.path.abspath(os.path.join('./','nih_sample/'))
+PATH = os.path.abspath(os.path.join('../','nih_sample/'))
 SOURCE_IMAGES = os.path.join(PATH,'images')
 images = glob(os.path.join(SOURCE_IMAGES,'*.png'))
 print(images[0:3])
 
 # lables 
 
-lables = pd.read_csv('./nih_sample/sample_labels.csv')
+lables = pd.read_csv('../nih_sample/sample_labels.csv')
 print(lables.head(3))
 
