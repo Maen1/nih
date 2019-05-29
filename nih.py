4d8ca3a54d56adab392636cef31c56cb878b567b
Maen1 <mk.maen93@gmail.com>
2019-05-29 23:09:30 +0800

    pre processing data

--- nih.py
+++ nih.py
@@ -1,15 +1,31 @@
-import os
-from glob import *
-import pandas as pd
 
+import os
+import matplotlib
+import numpy as np
+import pandas as pd
+from itertools import chain
+from glob import iglob, glob
+
+# %matplotlib inline
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 # path
-PATH = os.path.abspath(os.path.join('../','nih_sample/'))
-SOURCE_IMAGES = os.path.join(PATH,'images')
-images = glob(os.path.join(SOURCE_IMAGES,'*.png'))
-print(images[0:3])
+# PATH = os.path.abspath(os.path.join('../','nih_sample/'))
+# SOURCE_IMAGES = os.path.join(PATH,'images')
+# images = glob(os.path.join(SOURCE_IMAGES,'*.png'))
+# another way to get files
+all_image_paths = {os.path.basename(x): x for x in 
+                   glob(os.path.join('../input/*','images*', '*.png'))}
 
 # lables 
 
-lables = pd.read_csv('../nih_sample/sample_labels.csv')
-print(lables.head(3))
+dataframe = pd.read_csv('../nih_sample/sample_labels.csv')
+print(dataframe.shape)
+dataframe['path'] = dataframe['Image Index'].map(all_image_paths.get)
+dataframe['Patient Age'] = dataframe['Patient Age'].map(lambda x: int(x[:-1]))
+dataframe = dataframe[dataframe['Finding Labels'] != 'No Finding']
+all_labels = np.unique(list(chain(dataframe['Finding Labels'].map(lambda x: x.split('|')).tolist())))
+print(all_labels)
 
+dataframe = dataframe.drop(['Patient Age', 'Patient Gender', 'Follow-up #', 'Patient ID', 'View Position', 
+        'OriginalImageWidth', 'OriginalImageHeight', 'OriginalImagePixelSpacing_x','OriginalImagePixelSpacing_y'], axis=1)
