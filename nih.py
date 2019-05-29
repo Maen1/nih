58d6a680cb5c72576dc54eb7c7e67b668fe40667
Maen1 <mk.maen93@gmail.com>
2019-05-29 23:16:55 +0800

    update scritp

--- nih.py
+++ nih.py
@@ -18,14 +18,18 @@ all_image_paths = {os.path.basename(x): x for x in
                    glob(os.path.join('../input/*','images*', '*.png'))}
 
 # lables 
-
 dataframe = pd.read_csv('../nih_sample/sample_labels.csv')
 print(dataframe.shape)
 dataframe['path'] = dataframe['Image Index'].map(all_image_paths.get)
 dataframe['Patient Age'] = dataframe['Patient Age'].map(lambda x: int(x[:-1]))
 dataframe = dataframe[dataframe['Finding Labels'] != 'No Finding']
-all_labels = np.unique(list(chain(dataframe['Finding Labels'].map(lambda x: x.split('|')).tolist())))
-print(all_labels)
-
+all_labels = np.unique(list(chain(*dataframe['Finding Labels'].map(lambda x: x.split('|')).tolist())))
+pathology_list = all_labels
+dataframe['path'] = dataframe['Image Index'].map(all_image_paths.get)
 dataframe = dataframe.drop(['Patient Age', 'Patient Gender', 'Follow-up #', 'Patient ID', 'View Position', 
         'OriginalImageWidth', 'OriginalImageHeight', 'OriginalImagePixelSpacing_x','OriginalImagePixelSpacing_y'], axis=1)
+
+
+for pathology in pathology_list:
+    dataframe[pathology] = dataframe['Finding Labels'].apply(lambda x: 1 if pathology in x else 0)
+dataframe = dataframe.drop(['Image Index', 'Finding Labels'], axis=1)
