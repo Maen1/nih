5fea7673a6ac7c7afcc8dab2285c3e930a209df6
Maen1 <mk.maen93@gmail.com>
2019-06-03 23:06:43 +0800

    typoo

--- nih.py
+++ nih.py
@@ -89,7 +89,7 @@ parallel_model.summary()
 
 history = parallel_model.fit(X_train, y_train, epochs = 40, batch_size = 128, verbose=1, validation_data=(X_test, y_test))
 
-parallel.save('nih_model.h5')
+parallel_model.save('nih_model.h5')
 def history_plot(history):
     plt.plot(history.history['acc'])
     plt.plot(history.history['val_acc'])
