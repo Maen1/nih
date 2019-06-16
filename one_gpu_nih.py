2325b4c3374cff74e5544dc3afa68e1f4ea695e8
Maen1 <mk.maen93@gmail.com>
2019-06-16 19:04:15 +0800

    wip

--- one_gpu_nih.py
+++ one_gpu_nih.py
@@ -102,7 +102,7 @@ model.summary()
 
 history = model.fit(X_train, y_train, epochs = 30, verbose=1, validation_data=(X_test, y_test))
 
-model.save('../nih_sample/d121_nih_model.h5')
+model.save('../nih_sample/nih_model_10.h5')
 def history_plot(history):
     plt.plot(history.history['acc'])
     plt.plot(history.history['val_acc'])
@@ -131,7 +131,7 @@ for (idx, c_label) in enumerate(all_labels):
 c_ax.legend()
 c_ax.set_xlabel('False Positive Rate')
 c_ax.set_ylabel('True Positive Rate')
-fig.savefig('barely_trained_net.png')
+fig.savefig('barely_trained_net_10.png')
 
 
 sickest_idx = np.argsort(np.sum(y_test, 1)<1)
@@ -146,4 +146,4 @@ for (idx, c_ax) in zip(sickest_idx, m_axs.flatten()):
                              if (n_score>0.5) or (p_score>0.5)]
     c_ax.set_title('Dx: '+', '.join(stat_str)+'\nPDx: '+', '.join(pred_str))
     c_ax.axis('off')
-fig.savefig('trained_img_predictions.png')
+fig.savefig('trained_img_predictions_10.png')
