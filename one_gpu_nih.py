9827c0a7b7feec1432f1ec4a1850531cbdc342a3
Maen1 <mk.maen93@gmail.com>
2019-06-24 16:28:55 +0800

    01

--- one_gpu_nih.py
+++ one_gpu_nih.py
@@ -39,7 +39,7 @@ df_sample = dataframe.sample(frac = 0.50, random_state = 5)
 deasises = list(df_sample["Finding Labels"].unique())
 
 #train data set
-df_sample_train = df_sample.sample(frac = 0.75, random_state = 5)
+df_sample_train = df_sample.sample(frac = 0.01, random_state = 5)
 # isolated for the test
 df_sample_test = dataframe.drop(df_sample.index)
 
@@ -123,10 +123,10 @@ model.summary()
 
 history = model.fit(X_train, y_train, epochs = 100, batch_size=64, verbose=1, validation_split=0.2 , shuffle=True)
 
-model.save('../nih_sample/nih_model_50_75.h5')
+model.save('../nih_sample/nih_model_50_01.h5')
 def history_plot(history):
-    plt.plot(history.history['acc'])
-    plt.plot(history.history['val_acc'])
+    plt.plot(history.history['categorical_accuracy'])
+    plt.plot(history.history['val_categorical_accuracy'])
     plt.title('model accuracy')
     plt.ylabel('accuracy')
     plt.xlabel('epoch')
@@ -150,22 +150,22 @@ from sklearn.metrics import roc_curve, auc
 
 print(history.history.keys())
 # summarize history for accuracy
-plt.plot(history.history['acc'])
-plt.plot(history.history['val_acc'])
+plt.plot(history.history['categorical_accuracy'])
+plt.plot(history.history['val_categorical_accuracy'])
 plt.title('model accuracy')
 plt.ylabel('accuracy')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
-plt.savefig('./images/accuracy_50_75.png')
+plt.savefig('./images/accuracy_50_01.png')
 
 # summarize history for loss
 plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
+plt.plot(history.history['val_categorical_accuracy'])
 plt.title('model loss')
 plt.ylabel('loss')
 plt.xlabel('epoch')
 plt.legend(['train', 'test'], loc='upper left')
-plt.savefig('./images/loss_50_75.png')
+plt.savefig('./images/loss_50_01.png')
 
 
 fig, c_ax = plt.subplots(1,1, figsize = (9, 9))
@@ -175,7 +175,7 @@ for (idx, c_label) in enumerate(all_labels):
 c_ax.legend()
 c_ax.set_xlabel('False Positive Rate')
 c_ax.set_ylabel('True Positive Rate')
-fig.savefig('./images/trained_net_50_75.png')
+fig.savefig('./images/trained_net_50_01.png')
 
 
 sickest_idx = np.argsort(np.sum(y_test, 1)<1)
@@ -187,7 +187,7 @@ for (idx, c_ax) in zip(sickest_idx, m_axs.flatten()):
     for n_class, n_score, p_score in zip(all_labels, y_test[idx], predictions[idx]) if (n_score>0.5) or (p_score>0.5)]
     c_ax.set_title('Dx: '+', '.join(stat_str)+'\nPDx: '+', '.join(pred_str))
     c_ax.axis('off')
-fig.savefig('./images/trained_img_predictions_50_75.png')
+fig.savefig('./images/trained_img_predictions_50_01.png')
 
 
 
